1d9bd289993e2de360b799e89105d28cab6c075d
Kalanda Dmitry <dimayker2006@gmail.com>
2026-02-02 22:54:33 +0300

    Add base PlayerMovement script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _translationSpeed = 0f;
+    [SerializeField] private float _forcePower = 10f;
+    [SerializeField] private float _checkRadius = 5f;
+    [SerializeField] private Transform _groundCheck;
+    
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+        if(Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void Jump()
+    {   
+        if(Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, 1 << LayerMask.NameToLayer("Ground")))
+            _rb.AddForce(Vector2.up * _forcePower, ForceMode2D.Impulse);
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.right * (_translationSpeed * Time.deltaTime));
+    }
+}
