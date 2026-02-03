b579791a5ae6290b0cd15e709e645e7fdfa15d75
Kalanda Dmitry <dimayker2006@gmail.com>
2026-02-03 21:25:34 +0300

    Add death Action

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,46 +1,63 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float _translationSpeed;
-    [SerializeField] private float _maxVelocity;
-    [SerializeField] private float _forcePower;
-    [SerializeField] private float _checkRadius;
-    [SerializeField] private Transform _groundCheck;
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _translationSpeed;
+        [SerializeField] private float _maxVelocity;
+        [SerializeField] private float _forcePower;
+        [SerializeField] private float _checkRadius;
+        [SerializeField] private Transform _groundCheck;
+
+        private Vector2 _startPos;
+
+        public event Action OnPlayerDied; 
     
-    private Rigidbody2D _rb;
+        private Rigidbody2D _rb;
 
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _startPos = transform.position;
+            OnPlayerDied += Respawn;
+        }
 
-    private void FixedUpdate()
-    {
-        Move();
-    }
+        private void FixedUpdate()
+        {
+            Move();
+        }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-            Jump();
-    }
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                Jump();
+        }
 
-    private void Jump()
-    {   
-        if(Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, 1 << LayerMask.NameToLayer("Ground")))
-            _rb.AddForce(Vector2.up * _forcePower, ForceMode2D.Impulse);
-    }
+        private void Jump()
+        {   
+            if(Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, 1 << LayerMask.NameToLayer("Ground")))
+                _rb.AddForce(Vector2.up * _forcePower, ForceMode2D.Impulse);
+        }
 
-    private void Move()
-    {
-        var move = Vector2.right * (_translationSpeed * Time.fixedDeltaTime);
-        /*_rb.linearVelocity = Vector2.right * (_translationSpeed);*/
-        /*_rb.AddForce(move);*/
-        /*_rb.MovePosition(_rb.position + move);*/
-        transform.Translate(move);
+        private void Move()
+        {
+            var move = Vector2.right * (_translationSpeed * Time.fixedDeltaTime);
+            transform.Translate(move);
+        }
+
+        private void Respawn()
+        {
+            transform.position = _startPos;
+            Debug.Log("Плеер  здох!");
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.TryGetComponent<Trap>(out var trap))
+                OnPlayerDied?.Invoke();
+        }
     }
-    
 }
