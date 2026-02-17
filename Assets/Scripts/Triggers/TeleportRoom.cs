9e6b20b7122e003a99854cd632225b2a4cefc191
Kalanda Dmitry <dimayker2006@gmail.com>
2026-02-17 20:38:21 +0300

    add teleport trigger

--- Assets/Scripts/Triggers/TeleportRoom.cs
+++ Assets/Scripts/Triggers/TeleportRoom.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Player;
 using UnityEngine;
 
@@ -5,15 +6,32 @@ namespace Triggers
 {
     public class TeleportRoom : MonoBehaviour
     {
+
+        private PlayerMovement _player;
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.TryGetComponent<PlayerMovement>(out var player))
+            {
+                _player = player;
                 GoToNextRoom();
+            }
         }
 
         private void GoToNextRoom()
         {
-            
+            var spawnpoint = FindSpawnpoint();
+            _player.transform.position = spawnpoint.transform.position;
+            UnityEngine.Camera.main.transform.position += Vector3.right * 30;
+        }
+
+        private GameObject FindSpawnpoint()
+        {
+            var spawnpoints = GameObject.FindGameObjectsWithTag("Spawnpoint");
+            spawnpoints = spawnpoints
+                .OrderByDescending(x => x.transform.position.x)
+                .ToArray();
+            return spawnpoints[0];
         }
     }
 }
