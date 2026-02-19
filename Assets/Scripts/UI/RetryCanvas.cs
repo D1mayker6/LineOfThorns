7a35c9495ae618292b3bad225caef172ec339841
Kalanda Dmitry <dimayker2006@gmail.com>
2026-02-19 16:49:11 +0300

    minor changes(menu script)

--- Assets/Scripts/UI/RetryCanvas.cs
+++ Assets/Scripts/UI/RetryCanvas.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace UI
@@ -8,5 +9,21 @@ namespace UI
         [SerializeField] private Button _retryButton;
         [SerializeField] private Button _mainMenuButton;
         [SerializeField] private Text _scoreText;
+
+        private void Start()
+        {
+            _mainMenuButton.onClick.AddListener(Menu);
+            _mainMenuButton.onClick.AddListener(Retry);
+        }
+
+        private void Menu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        private void Retry()
+        {
+            
+        }
     }
 }
