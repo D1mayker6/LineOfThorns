5128e38117c36b08d076ceb33338dc7f3ca44b9b
Kalanda Dmitry <dimayker2006@gmail.com>
2026-07-08 16:59:08 +0300

    added start menu

--- Assets/Scripts/UI/MenuCanvas.cs
+++ Assets/Scripts/UI/MenuCanvas.cs
@@ -2,16 +2,31 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace UI
 {
     public class MenuCanvas : MonoBehaviour
     {
         [SerializeField] private Button _playButton;
+        [SerializeField] private GameObject _cinematicTexture;
+        [SerializeField] private GameObject _mainMenuPanel;
+        
+        private VideoPlayer _videoPlayer;
 
         private void Start()
         {
+            _mainMenuPanel.SetActive(false);
+            _videoPlayer = _cinematicTexture.GetComponentInChildren<VideoPlayer>();
+            _videoPlayer.Play();
+            _videoPlayer.loopPointReached += VideoPlayerOnloopPointReached;
             _playButton.onClick.AddListener(Play);
+        }
+
+        private void VideoPlayerOnloopPointReached(VideoPlayer source)
+        {
+            _cinematicTexture.gameObject.SetActive(false);
+            _mainMenuPanel.SetActive(true);
         }
 
         private void Play()
