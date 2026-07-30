970e23536798af5e393c2f67a8510fe553b51190
Kalanda Dmitry <dimayker2006@gmail.com>
2026-07-30 14:10:10 +0300

    timer ui and anim added

---
+++ Assets/Scripts/UI/ExtralifeTime.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ExtralifeTime : MonoBehaviour
+    {
+
+        [SerializeField] private Image _timer;
+        [SerializeField] private TextMeshProUGUI _timerText;
+        [SerializeField] private Button _button;
+    }
+}
