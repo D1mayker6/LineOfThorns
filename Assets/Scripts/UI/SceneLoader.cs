0972a00787949694a982baa2b4b875daf25e6c83
Kalanda Dmitry <dimayker2006@gmail.com>
2026-07-30 10:56:54 +0300

    add blackscreen

--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -6,8 +6,11 @@ using UnityEngine.SceneManagement;
 namespace UI
 {
     public class SceneLoader : MonoBehaviour {
-    
+        private static readonly int Start = Animator.StringToHash("start");
+        private static readonly int End = Animator.StringToHash("end");
+
         private int _scene;
+        [SerializeField] private Animator _animator;
         public void LoadNewScene(int scene)
         {
             _scene = scene;
@@ -16,15 +19,21 @@ namespace UI
         }
 
 
-        IEnumerator LoadNewSceneAsync() {
+        IEnumerator LoadNewSceneAsync()
+        {
+            _animator.SetTrigger(Start);
+            yield return new WaitForSeconds(1f);
+            
 
-            yield return new WaitForSeconds(3);
-        
             var async = SceneManager.LoadSceneAsync(_scene);
+            async.allowSceneActivation = false;
 
-            while (!async.isDone) {
+            while (async.progress < 0.9f) {
                 yield return null;
             }
+            _animator.SetTrigger(End);
+            yield return new WaitForSeconds(1f);
+            async.allowSceneActivation = true;
 
         }
 
